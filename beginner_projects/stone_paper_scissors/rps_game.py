7db83c99c015cba55232e0b2b875e1d24a04e16f
Shoaib Codes <90686438+ShoaibLabs@users.noreply.github.com>
2025-07-12 16:49:45 -0400

    Add files via upload

---
+++ beginner_projects/stone_paper_scissors/rps_game.py
@@ -0,0 +1,71 @@
+import os
+import time
+import random
+
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
+
+choices = {
+    'r': 1,
+    'rock':1,
+    'stone':1,
+    '1' : 1,
+    '1.0' : 1,
+    
+    'p': 0,
+    'paper':0,
+    '2': 0,
+    '2.0' : 0,
+    
+    's':-1,
+    'scissors':-1,
+    '3' : -1,
+    '3.0': -1,
+}
+
+items = {
+    1 : "Rock",
+    0 : "Paper",
+    -1 : "Scissors"
+}
+
+player_score = 0
+computer_score = 0
+
+while True:
+    clear()
+    comp = random.choice([0,1,-1]) 
+    userchoice = input("Enter your choice\nRock/Paper/Scissors or q to quit: ").lower().strip()
+    print()
+
+
+    if userchoice in ["q", "quit"]:
+        print("Exiting the program...")
+        break
+
+    if userchoice not in choices:
+        print("Invalid Value. Try again!")
+        time.sleep(0.4)
+        continue
+    user = choices[userchoice]
+    print(f"Computer's choice: {items[comp]}")
+    time.sleep(0.4)
+    print(f"Your choice: {items[user]}")
+    print()
+    time.sleep(0.4)
+
+    if user == comp:
+        print("Its a Draw!")
+
+    else:
+        if (comp == 1 and user == 0) or (comp == 0 and user == -1) or (comp == -1 and user == 1):
+            player_score += 1
+            print("You Won!\n")
+        else:
+            computer_score += 1
+            print('Computer Won!\n')
+            
+    print(f'Your score: {player_score}\nComputer\'s score: {computer_score}\n')
+    time.sleep(0.4)
+    input("Press Enter to continue...")
+    continue
