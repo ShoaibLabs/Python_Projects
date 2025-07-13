d4d7d38d59f709aacb4687271e29149b1a8f0100
Shoaib Codes <90686438+ShoaibLabs@users.noreply.github.com>
2025-07-13 10:40:03 -0400

    Update counter_gui.py

--- beginner_projects/tkinter_counter_gui/counter_gui.py
+++ beginner_projects/tkinter_counter_gui/counter_gui.py
@@ -35,51 +35,64 @@ display = Label(display_frame,
 display.pack()
 
 #FUNCTIONS
+
+
 def background_color():
+    '''
+    Light and Dark modes.
+    '''
     global bgclr
     if bgclr == '#DFCFCF':
         bgclr = '#121212'
         display_frame.config(bg = bgclr)
         button_frame.config(bg = bgclr)
-        theme.config(bg = bgclr, fg = "#FFFFFF")
-        counter_button.config(bg = bgclr, fg = "#FFFFFF")
+        theme_button.config(bg = bgclr, fg = "#FFFFFF")
+        increment_button.config(bg = bgclr, fg = "#FFFFFF")
         reset_button.config(bg = bgclr, fg = "#FFFFFF")
         root.config(bg = bgclr)
     else:
         bgclr = '#DFCFCF'
         display_frame.config(bg = bgclr)
         button_frame.config(bg = bgclr)
-        theme.config(bg = bgclr, fg = "#000000")
-        counter_button.config(bg = bgclr, fg = "#000000")
+        theme_button.config(bg = bgclr, fg = "#000000")
+        increment_button.config(bg = bgclr, fg = "#000000")
         reset_button.config(bg = bgclr, fg = "#000000")
         root.config(bg = bgclr)
 
+
 def counter():
+    '''
+    Increment counter and update display.
+    '''
     global count
     count += 1
     if count % 10000 == 0:
         count = 0
     display.config(text = f'{count}')
 
+
 def reset():
+    '''
+    Reset the display to 0
+    '''
     global count
     count = 0
     display.config(text = f'{count}')
 
 # BUTTONS
-theme = Button(display_frame,
+theme_button = Button(display_frame,
                bg = bgclr,
                text = "Theme",
                command = background_color)
-theme.pack()
+theme_button.pack()
 
-counter_button = Button(button_frame,
+increment_button = Button(button_frame,
                         bg = bgclr,
                         text = "Count +1",
                         font = ('Comic Sans', 25),
                         command = counter
                         )
-counter_button.pack(pady = 50)
+increment_button.pack(pady = 50)
 
 reset_button = Button(button_frame,
                       bg = bgclr,
